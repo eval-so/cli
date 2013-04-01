1b15113f27e69ec48e4d32d53078f57cd177a064
Ricky Elrod <ricky@elrod.me>
2013-04-01 02:56:59 -0400

    Initial program. Needs a lot more error handling.

---
+++ evalgd.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+var (
+	action   = flag.String("action", "evaluate", "What should I do?")
+	language = flag.String("language", "", "Language to evaluate")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: evalgd [flags]\n")
+	fmt.Fprintf(os.Stderr, "Defaults are:\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		flag.Usage()
+	}
+
+	fmt.Println("Requesting evaluation...")
+	code, _ := ioutil.ReadAll(os.Stdin)
+	e := Evaluation{
+		Language: *language,
+		Code:     string(code),
+	}
+
+	result, statusCode := evaluate(e)
+
+	if statusCode != 200 {
+		fmt.Printf("[\x1b[31;1mError\x1b[0m] HTTP Code %d\n", statusCode)
+	} else {
+		fmt.Println(string(result))
+	}
+}
+
+type Evaluation struct {
+	Language string `json:"language"`
+	Code     string `json:"code"`
+}
+
+func evaluate(e Evaluation) (string, int) {
+	jsonQuery, _ := json.Marshal(e)
+	query := bytes.NewBuffer(jsonQuery)
+	resp, _ := http.Post(
+		"http://eval.gd/jsontest",
+		"application/json",
+		query)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	return string(body), resp.StatusCode
+}
